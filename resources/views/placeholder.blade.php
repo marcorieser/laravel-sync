89a946725cba000e35ff5157720c2c70b3e8e76d
Marco Rieser <marco@rieser.dev>
2026-07-22 21:15:32 +0200

    chore: remove placeholder package scaffold

--- resources/views/placeholder.blade.php
+++
@@ -1,1 +0,0 @@
-<div>Sync placeholder view.</div>
--- routes/laravel-sync.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// use Illuminate\Support\Facades\Route;
-
-// Route::get('laravel-sync', fn () => 'Sync placeholder route.')->name('laravel-sync.placeholder');
